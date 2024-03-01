198b281feff2042717eb16b61d36dfdb8e7fc9d3
jiwon <semanticist0@gmail.com>
2024-03-01 21:08:51 +0900

    🐛 버그 수정

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,21 @@
 const gulp = require("gulp");
 const ts = require("gulp-typescript");
+const merge = require("merge2");
 const terser = require("gulp-terser");
 
-const tsProject = ts.createProject("tsconfig.json");
+const tsProject = ts.createProject("tsconfig.json", { declaration: true });
 
-gulp.task("scripts", () => {
-  return tsProject
-    .src() // TypeScript 파일 위치
-    .pipe(tsProject()) // TypeScript 컴파일
-    .js // 컴파일된 JavaScript 파일 가져오기
-    .pipe(terser()) // JavaScript 파일 minify
-    .pipe(gulp.dest("dist")); // minify된 파일을 저장할 위치
+gulp.task("compile", () => {
+  const tsResult = gulp.src(["lib/**/*.ts", "lib/**/*.tsx"]).pipe(tsProject());
+
+  return merge([
+    tsResult.dts.pipe(gulp.dest("dist")), // Generate declaration files
+    tsResult.js.pipe(gulp.dest("dist")), // Generate JavaScript files
+  ]);
 });
 
-gulp.task("default", gulp.series("scripts"));
+gulp.task("minify", () => {
+  return gulp.src("dist/**/*.js").pipe(terser()).pipe(gulp.dest("dist"));
+});
+
+gulp.task("default", gulp.series("compile", "minify"));
